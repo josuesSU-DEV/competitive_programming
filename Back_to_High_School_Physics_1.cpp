7f2376fc0148f48f88506e44f0fe73de70fbfff9
josueSU-UNSA <82920949+josueSU-UNSA@users.noreply.github.com>
2022-09-26 23:36:05 -0500

    Add files via upload

---
+++ Back_to_High_School_Physics_1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int  v, t;
+    while(cin>>v>>t)
+    {
+             if(v && t)cout<<2*v*t<<endl;
+             else cout<<0<<endl;
+    }
+    return 0;
+}
